02d9b4b9874564d68f4f34987564637d1c52b014
Owen Wang <baiwen@ualberta.ca>
2022-11-30 13:45:51 -0700

    include CKDIV8 for ext osc.

--- owen.c
+++ owen.c
@@ -7,7 +7,7 @@
 #include "hd44780.h"
 #include "lcd.h"
 
-//ext osc. 20MHz
+//ext osc. 20MHz //CKDIV8
 
 
 int main(void){
@@ -24,9 +24,17 @@ int main(void){
     
     //initialize PWM, N=8
     ICR1 = 1250; //TOP value
-    OCR1A = 625; //75% duty cycle at equilibrium
+    OCR1A = 625; //50% duty cycle at equilibrium
     TCCR1A = (1<<COM1B1)|(1<<WGM11); //generate waveform in normal mode
-    TCCR1B = (1<<WGM13)|(1<<CS11); //(WGM13:10 = 0b1010), (CS12:10 = 0b010) 
+    TCCR1B = (1<<WGM13)|(1<<CS10); //(WGM13:10 = 0b1010), (CS12:10 = 0b001) 
 
     return (0);
 }
+
+/*
+freq calc
+f_PWM = f_clk/(2*N*TOP)
+f_PWM = 1ms = 1000Hz
+f_clk = 20MHz/8 = 2.5MHz
+N*TOP = 1250 ------> N = 1
+*\
