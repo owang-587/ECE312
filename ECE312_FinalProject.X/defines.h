1692aaf076f1025f684894c5807685883c1dd85c
jjshin1 <james.shin002@gmail.com>
2022-12-04 21:11:27 -0700

    changing pins in defines.h
    Changed pins to the new schematic

--- ECE312_FinalProject.X/defines.h
+++ ECE312_FinalProject.X/defines.h
@@ -19,13 +19,13 @@
 /* HD44780 LCD port connections */
 #define HD44780_RS D, 0
 #define HD44780_RW D, 1
-#define HD44780_E  C, 2
+#define HD44780_E  C, 4
 
 /* The data bits have to be not only in ascending order but also consecutive. */
-#define HD44780_D4 C, 3
-#define HD44780_D5 C, 4
-#define HD44780_D6 C, 5
-#define HD44780_D7 C, 6
+#define HD44780_D4 C, 0
+#define HD44780_D5 C, 1
+#define HD44780_D6 C, 2
+#define HD44780_D7 C, 3
 
 /* Whether to read the busy flag, or fall back to worst-time delays. */
 #define USE_BUSY_BIT 1
